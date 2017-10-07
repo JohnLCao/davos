4c7da3689c6658dfce7b517fe404a8dfe32ebd3b
John L. Cao <john_caohaha@hotmail.com>
2017-10-06 20:25:48 -0700

    more interface stuff

--- priority_queue.rb
+++ priority_queue.rb
@@ -3,8 +3,23 @@ require 'json'
 class PriorityQueue
 	attr_accessor :data
 	@@file_path = './data/data.json'
+
 	def initialize
 		file_string = File.read(@@file_path)
 		@data = JSON.parse(file_string)
 	end
+
+	def insert
+	end
+
+	def remove
+	end
+
+	def list
+	end
+
+	private
+		def sort
+		end
+
 end
--- repl.rb
+++ repl.rb
@@ -1,8 +1,12 @@
 require './priority_queue.rb'
+require 'colorize' # make CLI prettier eventually
 
 pq = PriorityQueue.new
 
+# main loop
 begin
-	puts "Y/N"
+	puts "L(ist), I(nsert), R(emove), Q(uit)"
 	flag = gets.strip
-end until flag.downcase == 'n'
+end until flag.downcase == 'q'
+
+puts "Best of luck!"
