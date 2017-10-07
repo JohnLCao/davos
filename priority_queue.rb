4f4d5b0a21c0f632f48b12f1d6a716f0c419f67b
John L. Cao <john_caohaha@hotmail.com>
2017-10-06 20:59:12 -0700

    test data as well as sorting function

--- priority_queue.rb
+++ priority_queue.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'date'
 
 class PriorityQueue
 	attr_accessor :data
@@ -7,9 +8,11 @@ class PriorityQueue
 	def initialize
 		file_string = File.read(@@file_path)
 		@data = JSON.parse(file_string)
+		sort
 	end
 
 	def insert
+		
 	end
 
 	def remove
@@ -18,8 +21,16 @@ class PriorityQueue
 	def list
 	end
 
+	def quit
+	end
+
 	private
 		def sort
+			@data.sort_by!{ |t| sort_helper(t) }
 		end
 
+		# customizable
+		def sort_helper task
+			return Date.parse(task["due-date"])
+		end
 end
